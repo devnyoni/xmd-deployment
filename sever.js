63027b73064d6736518592d1c5a5b2c911f379e7
devnyoni <devnyoni@gmail.com>
2025-12-31 09:20:55 +0300

    Create sever.js

---
+++ sever.js
@@ -0,0 +1,14 @@
+ // server.js
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'deploy.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Deployment Dashboard running at http://localhost:3000');
+});
